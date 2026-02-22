287cb643989c616b27951efbcf5ce1d79a73ae61
Jack Murphy <jackmurphy669@gmail.com>
2026-02-21 22:44:07 -0700

    Add two-soliton collision demo script

---
+++ scripts/run_two_soliton_collusion.m
@@ -0,0 +1,12 @@
+% Run two-soliton collision demo
+
+addpath(genpath(fullfile(fileparts(mfilename('fullpath')), '..', 'src')));
+
+p = nls_default_params();
+p.ic   = "two_soliton";
+p.tmax = 5;
+
+out = nls_solver_ifrk4(p);
+
+nls_plot_solution(out);
+nls_plot_mass_error(out);
